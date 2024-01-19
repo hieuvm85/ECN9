8e1922b5b8133197023cecbac81d1f8259c7db8f
No0bIT <unique08052002@gmail.com>
2024-01-19 21:40:14 +0700

    create source

---
+++ src/main/java/com/example/Ecommerce_BE/EcommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBeApplication.class, args);
+	}
+
+}
